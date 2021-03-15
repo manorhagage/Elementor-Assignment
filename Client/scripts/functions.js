ed0627306216f862f1fdf99d77acd3543461529b
manor hagage <manorhagage@gmail.com>
2021-03-15 08:47:30 +0200

    Client - Add helper functions

---
+++ Client/scripts/functions.js
@@ -0,0 +1,15 @@
+
+// get one element by CSS selector
+function _( selector )
+{
+	return document.querySelector( selector );
+
+}
+
+// get all elements by CSS selector
+function __( selector )
+{
+	return document.querySelectorAll( selector );
+
+}
+
